41bb09c303eaf129c961f3ce47f2ad6788a1ce86
hj <linxu1732@163.com>
2019-08-19 17:44:14 +0800

    Initial commit

---
+++ src/test/java/com/cc/security/SecurityApplicationTests.java
@@ -0,0 +1,19 @@
+package com.cc.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        ArrayList<String> a = new ArrayList<>();
+    }
+
+}
